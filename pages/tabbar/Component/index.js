7bd0544becf696fc346b668c5b6696f0e72bcdef
wangxianliang <wangxianliang@jinrongweb.com>
2018-05-24 17:30:42 +0800

    初始化小程序工程项目

---
+++ pages/tabbar/Component/index.js
@@ -0,0 +1,105 @@
+// pages/tabbar/Component/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        id:'view',
+        name:"视图容器",
+        open:false,
+        pages:['view','scroll-view','swiper']
+      },{
+        id:'content',
+        name:"基础内容",
+        open:false,
+        pages:['text','icon','progress']
+      },{
+        id:'form',
+        name:'表单组件',
+        open:false,
+        pages:['button','checkbox','form',
+        'input','label','picker','radio','slider',
+        'switch','textarea']
+      },{
+        id:'nav',
+        name:'导航',
+        open:false,
+        pages:['naviagator']
+      },{
+        id:'media',
+        name:'媒体组件',
+        open:false,
+        pages:['image','audio','video']
+      },{
+        id:'map',
+        name:'地图',
+        open:false,
+        pages:['map']
+      },{
+        id:'canvas',
+        name:'画布',
+        open:false,
+        pages:['canvas']
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
